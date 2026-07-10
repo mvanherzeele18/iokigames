13b2e5084628fce870526473982979a66c792f4b
mvanherzeele18 <vanherzeelematteo@gmail.com>
2026-07-10 13:32:40 +0200

    Update train.js

--- age-4/train.js
+++ age-4/train.js
@@ -40,10 +40,9 @@ startButton.addEventListener("click", () => {
 
     startButton.disabled = true;
 
-    const distance = window.innerWidth + train.offsetWidth + 200;
+    const distance = train.offsetWidth + 400;
 
-    train.style.transition = "transform 4s linear";
-    train.style.transform = `translateX(${distance}px)`;
+    train.style.transform = `translateX(-${distance}px)`;
 
     setTimeout(() => {
 
@@ -56,9 +55,9 @@ startButton.addEventListener("click", () => {
             >
         `;
 
-        // Trein links buiten beeld zetten
+        // Trein rechts buiten beeld zetten
         train.style.transition = "none";
-        train.style.transform = "translateX(-300px)";
+        train.style.transform = `translateX(${window.innerWidth + 300}px)`;
 
         // Browser de tijd geven om de positie toe te passen
         requestAnimationFrame(() => {
